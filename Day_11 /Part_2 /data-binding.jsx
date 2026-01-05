058078db4f45623a58cf457e1bbca5d88a0225ba
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-01-05 20:59:30 +0530

    Implement DataBinding component with menu items

--- Day_11 /Part_2 /data-binding.jsx
+++ Day_11 /Part_2 /data-binding.jsx
@@ -1,1 +1,26 @@
+export function DataBinding() {
+  var Menu = [
+    { Category: "Electronics", Products: ["Television", "Mobile", "Watches"] },
+    {
+      Category: "Fashion",
+      Products: ["Men's Fashion", "Kids Fashion", "Women Fashion"],
+    },
+  ];
+  return (
+    <>
+      <ol>
+        {Menu.map((item) => (
+          <li key={item.Category}>
+            {item.Category}
+            <ul>
+              {item.Products.map((product) => (
+                <li key={product}>{product}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ol>
+    </>
+  );
+}
 
