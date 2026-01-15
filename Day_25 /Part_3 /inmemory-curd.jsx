e3f51567369d834658d10bd7c3533f22bcabaec5
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-01-15 11:09:50 +0530

    Create inmemory-curd.jsx

---
+++ Day_25 /Part_3 /inmemory-curd.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+
+export function InMemoryCURD() {
+  const [products, setProducts] = useState([
+    { id: 1, Name: "TV" },
+    { id: 2, Name: "Mobile" },
+  ]);
+
+  const [newProduct, setNewProduct] = useState({ id: 0, Name: "" });
+
+  function handleIdChange(e) {
+    setNewProduct({
+      id: e.target.value,
+      Name: newProduct.Name,
+    });
+  }
+
+  function handleNameChange(e) {
+    setNewProduct({
+      Name: e.target.value,
+      id: newProduct.id,
+    });
+  }
+  function addItemClick() {
+    products.push(newProduct);
+    setProducts(products.filter((product) => product.id !== 0));
+    alert(`Total items are: ${products.length}`);
+  }
+  function deleteItemClick(id) {
+    setProducts(products.filter((product) => product.id !== id));
+    alert(`Product deleted ${id}`);
+  }
+
+  return (
+    <div className="container-fluid">
+      <h2>Testing CRUD</h2>
+      <div>
+        <label className="fw-bold">Add New Product</label>
+        <div className="w-25">
+          <dl>
+            <dt>Product Id</dt>
+            <dd>
+              <input
+                type="number"
+                className="form-control"
+                min="1"
+                onChange={handleIdChange}
+              />
+            </dd>
+            <dt>Product Name</dt>
+            <dd>
+              <input
+                type="text"
+                className="form-control"
+                onChange={handleNameChange}
+              />
+            </dd>
+          </dl>
+          <button className="btn btn-success" onClick={addItemClick}>
+            {" "}
+            Add Product
+          </button>
+        </div>
+      </div>
+      <table className="table table-hover table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.Name}</td>
+              <td>
+                <button className="btn btn-warning bi bi-pencil-square me-2 "></button>
+                <button
+                  className="btn btn-danger bi bi-trash-fill"
+                  onClick={() => deleteItemClick(product.id)}
+                ></button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
