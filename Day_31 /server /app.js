af676e99667be1ab5f152c04c03a8309f1a16103
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-01-23 15:50:28 +0530

    Initialize Express server with CRUD endpoints
    Set up Express server with MongoDB integration for managing appointments.

---
+++ Day_31 /server /app.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const mongoClient = require("mongodb").MongoClient;
+const cors = require("cors");
+
+var connectionString = "mongodb://127.0.0.1:27017";
+
+var app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`<h2>To DO - API</h2>`);
+});
+
+app.get("/appointments", (req, res) => {
+  mongoClient.connect(connectionString).then((ClientObj) => {
+    var database = ClientObj.db("todo");
+    database
+      .collection("appointments")
+      .find({})
+      .toArray()
+      .then((documents) => {
+        res.send(documents);
+        res.end();
+      });
+  });
+});
+
+app.get("/appointments/:id", (req, res) => {
+  const Id = parseInt(req.params.id);
+  mongoClient.connect(connectionString).then((ClientObj) => {
+    const database = ClientObj.db("todo");
+    database
+      .collection("appointments")
+      .findOne({ Id: Id })
+      .then((iddocument) => {
+        res.send(iddocument);
+        res.end();
+      });
+  });
+});
+
+app.post("/addtask", (req, res) => {
+  const data = {
+    Id: parseInt(req.body.Id),
+    Title: req.body.Title,
+    Date: new Date(req.body.Date),
+    Description: req.body.Description,
+  };
+  mongoClient.connect(connectionString).then((ClientObj) => {
+    var database = ClientObj.db("todo");
+    database
+      .collection("appointments")
+      .insertOne(data)
+      .then(() => {
+        console.log("one record inserted successfully");
+        res.status(201).send("Task Added Successfully");
+      });
+  });
+});
+
+app.put("/edittask/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  mongoClient.connect(connectionString).then((ClientObj) => {
+    var database = ClientObj.db("todo");
+    database
+      .collection("appointments")
+      .updateOne(
+        { Id: id },
+        {
+          $set: {
+            Id: id,
+            Title: req.body.Title,
+            Date: new Date(req.body.Date),
+            Description: req.body.Description,
+          },
+        },
+      )
+      .then(() => {
+        console.log("Data updated Successfully");
+        res.status(200).send("Record Updated Successfully");
+      });
+  });
+});
+
+app.delete("/deletetask/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  mongoClient.connect(connectionString).then((ClientObj) => {
+    var database = ClientObj.db("todo");
+    database
+      .collection("appointments")
+      .deleteOne({ Id: id })
+      .then(() => {
+        console.log(`Record deleted successfully`);
+        res.status(200).send("Record Deleted Successfully");
+      });
+  });
+});
+
+const Port = 4000;
+app.listen(Port, () => {
+  console.log(`Server is listening on : http://127.0.0.1:${Port}`);
+});
