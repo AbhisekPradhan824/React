03962c030a45b67a83b9a50744be2ab2fcb10fdd
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-01-26 14:42:23 +0530

    Implement PropsDemo with UserLogin components
    Added two UserLogin components with different props for user authentication.

--- Day_33 /Part_2 /prop-demo.jsx
+++ Day_33 /Part_2 /prop-demo.jsx
@@ -1,1 +1,29 @@
 
+import { UserLogin } from "../../components-library/userLogin";
+
+export function PropsDemo() {
+  return (
+    <div className="container-fluid">
+      <h1>Shopping Home</h1>
+      <UserLogin
+        Title="Enter UserId and Password"
+        userLabel="Enter Email"
+        userType="text"
+        Verify="Password"
+        VerifyType="password"
+        Theme="w-25 bg-primary text-white p-2 text-center"
+        ButtonType="btn btn-light w-50"
+      />
+      <hr />
+      <UserLogin
+        Title="Enter email and code"
+        userLabel="Enter Email"
+        userType="email"
+        Verify="Code"
+        VerifyType="number"
+        Theme="w-50 bg-warning text-white p-2 text-center"
+        ButtonType="btn btn-danger w-50"
+      />
+    </div>
+  );
+}
