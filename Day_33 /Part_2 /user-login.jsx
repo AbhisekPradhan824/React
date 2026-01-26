8acb3323a2aaed5c229cb1042eb9ce051673da38
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-01-26 14:42:46 +0530

    Add UserLogin component for user authentication

--- Day_33 /Part_2 /user-login.jsx
+++ Day_33 /Part_2 /user-login.jsx
@@ -1,1 +1,19 @@
 
+export function UserLogin(props) {
+  return (
+    <div className="container-fluid">
+      <dl className={props.Theme}>
+        <h3>{props.Title}</h3>
+        <dt>{props.userLabel}</dt>
+        <dd>
+          <input type={props.userType} className="form-control w-100" />
+        </dd>
+        <dt>{props.Verify}</dt>
+        <dd>
+          <input type={props.VerifyType} className="form-control w-100" />
+        </dd>
+        <button className={props.ButtonType}>Login</button>
+      </dl>
+    </div>
+  );
+}
