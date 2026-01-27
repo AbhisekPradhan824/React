25fb5f88632f1745815d0141e53bd29f82b656d3
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-01-27 12:00:43 +0530

    Update props-demo.jsx

--- Day_34 /part 1 /props-demo /props-demo.jsx
+++ Day_34 /part 1 /props-demo /props-demo.jsx
@@ -1,1 +1,39 @@
 
+import { DataGrid } from "../../components-library/data-grid";
+
+export function PropsDemo() {
+  return (
+    <div className="container-fluid">
+      <h1>Employee Details</h1>
+      <DataGrid
+        caption="Employee Data"
+        fields={["Employee Name", "Employee Id", "Salary"]}
+        tablestyle="table-boder"
+        data={[
+          { Empname: "Salini Khusi", empId: 102, Salary: 50000 },
+          { Empname: "Abhisek Pradhan", empId: 103, Salary: 52000 },
+        ]}
+      />
+      <hr />
+      <DataGrid
+        caption="Student Data"
+        fields={["Student Name", "Student Id", "Branch", "Semester"]}
+        tablestyle="table-primary table-striped"
+        data={[
+          {
+            StdName: "Salini Khusi",
+            stdId: 102,
+            Branch: "B.ed",
+            Semester: "3rd",
+          },
+          {
+            StdName: "Abhisek Pradhan",
+            stdId: 103,
+            Branch: "B.Tech",
+            Semester: "4th",
+          },
+        ]}
+      />
+    </div>
+  );
+}
