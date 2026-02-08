a420833086257a8c113163c0efb527835a8da285
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-02-08 20:50:18 +0530

    Add ClassEventDemo component with insert functionality

---
+++ Day_38 /Part_2 /class-event-demo /class-event-demo.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export class ClassEventDemo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      msg: "",
+    };
+  }
+  InsertClick() {
+    this.setState({
+      msg: "Record inserted",
+    });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container-fluid">
+          <button onClick={() => this.InsertClick()}>Insert</button>
+          <p>{this.state.msg}</p>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
