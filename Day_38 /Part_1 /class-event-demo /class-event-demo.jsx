bf38efed2aa91f554f9b5ea2919b71b8aeb60e66
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-02-08 12:29:28 +0530

    Update class-event-demo.jsx

--- Day_38 /Part_1 /class-event-demo /class-event-demo.jsx
+++ Day_38 /Part_1 /class-event-demo /class-event-demo.jsx
@@ -1,1 +1,27 @@
-c
+import React from "react";
+
+export class ClassEventDemo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      msg: "",
+    };
+    this.InsertClick = this.InsertClick.bind(this);
+  }
+  InsertClick() {
+    this.setState({
+      msg: "Record inserted",
+    });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container-fluid">
+          <button onClick={this.InsertClick}>Insert</button>
+          <p>{this.state.msg}</p>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
