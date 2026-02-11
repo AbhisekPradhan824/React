5f02e128f7911554a024009c6acfb191b1c03cc9
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-02-11 13:53:28 +0530

    Create App.js

---
+++ Day-41 /react-router /src /App.js
@@ -0,0 +1,61 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { Login } from "./components/login/login";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <header>
+          <h2>Shopper.</h2>
+          <nav>
+            <Link to="/">Home</Link> <span></span>
+            <Link to="kids">Kids</Link>
+            <span></span>
+            <Link to="men">Men</Link>
+            <span></span>
+            <Link to="login">Login </Link>
+          </nav>
+        </header>
+        <hr />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h2>Home</h2> <p>Year end Sale 40% OFF</p>
+              </>
+            }
+          ></Route>
+          <Route
+            path="kids"
+            element={
+              <>
+                <h2>Kids Fashion</h2> <p>30% OFF on kids wear</p>
+              </>
+            }
+          ></Route>
+          <Route
+            path="men"
+            element={
+              <>
+                <h2>Men's Fashion</h2> <p>winter wear, Shoes , Jeans...</p>
+              </>
+            }
+          ></Route>
+          <Route path="login" element={<Login />} />
+          <Route
+            path="*"
+            element={
+              <>
+                <code>Not Found: Path you request is not found</code>
+              </>
+            }
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
