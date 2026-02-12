bfc14488f1eb92f7d5d77b18868bcd5906c85e2b
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-02-12 13:47:41 +0530

    Create login.jsx

---
+++ Day_42 /src /components /login /login.jsx
@@ -0,0 +1,9 @@
+export function Login() {
+  return (
+    <>
+      <input type="text" placeholder="enter user name" />
+      <input type="password" placeholder="Enter password" />
+      <button>Submit</button>
+    </>
+  );
+}
