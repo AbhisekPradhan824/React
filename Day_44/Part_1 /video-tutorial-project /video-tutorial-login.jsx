6753be6aed3b83edd8b88af7d628231f8eb4271c
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-02-17 00:51:00 +0530

    Update video-tutorial-login.jsx

--- Day_44/Part_1 /video-tutorial-project /video-tutorial-login.jsx
+++ Day_44/Part_1 /video-tutorial-project /video-tutorial-login.jsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
 export function TutorialLogin() {
   return (
     <div>
-      <h3>User Login</h3>
+      <form className="w-25">
+        <h3>User Login</h3>
+        <dl>
+          <dt>User Id</dt>
+          <dd>
+            <input type="text" className="form-control" name="UserId" />
+          </dd>
+          <dt>Password</dt>
+          <dd>
+            <input type="password" className="form-control" />
+          </dd>
+        </dl>
+        <button className="btn btn-primary">Login</button>
+      </form>
+      <p>
+        <Link to="/register">New User Register</Link>
+      </p>
     </div>
   );
 }
