9b306af853fbc4406ad97290ced3289555cd4e73
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-02-23 11:29:08 +0530

    Create index.ts

---
+++ Day_48/Part_2/index.ts
@@ -0,0 +1,20 @@
+function Total(qty: number, price: number): number {
+  return qty * price;
+}
+function Print(
+  Name: string,
+  Price: number,
+  Qty: number,
+  Rating?: number,
+): void {
+  if (Rating) {
+    console.log(
+      `Name: ${Name} \n Price: ${Price} \n Qty: ${Qty} \n Total: ${Total(Qty, Price)} \n Rating: ${Rating}`,
+    );
+  } else {
+    console.log(
+      `Name: ${Name} \n Price: ${Price} \n Qty: ${Qty} \n Total: ${Total(Qty, Price)}`,
+    );
+  }
+}
+Print("Samsung TV", 45000.76, 3);
