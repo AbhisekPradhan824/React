55c149acab8c835b17c9a6681bd00ec8ec810512
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-02-23 12:05:18 +0530

    Create index.ts

---
+++ Day_48/Part_3/index.ts
@@ -0,0 +1,21 @@
+interface IProduct {
+  Name: string;
+  Price: number;
+  Qty: number;
+  Total(): number;
+  Print(): void;
+}
+
+let product: IProduct = {
+  Name: "Samsung TV",
+  Price: 12000,
+  Qty: 3,
+  Total: function () {
+    return this.Qty * this.Price;
+  },
+  Print: function () {
+    console.log(`
+    Name:  ${this.Name}\n Price: ${this.Price} \n Qty: ${this.Qty} \n Total: ${this.Total()}`);
+  },
+};
+product.Print();
