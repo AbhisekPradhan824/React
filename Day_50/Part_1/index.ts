b0ea57b50cce3ebfd2b09c9fee5470ae3a512b13
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-02-25 13:12:06 +0530

    Update index.ts

--- Day_50/Part_1/index.ts
+++ Day_50/Part_1/index.ts
@@ -1,5 +1,12 @@
+interface IProduct {
+  Name: string;
+  Price: number;
+}
+
 function Print<T>(data: T): T {
   return data;
 }
 console.log(Print<string>("Hello"));
 console.log(Print<number>(20));
+console.log(Print<IProduct>({ Name: "Samsung TV", Price: 12999 }));
+console.log(Print<string[]>(["Abh", "ans", "A", "B", "C"]));
