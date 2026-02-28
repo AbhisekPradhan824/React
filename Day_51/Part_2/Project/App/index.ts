916a7946fc25e87e80f7d83a5d531d90f1575388
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-02-28 11:43:04 +0530

    Create index.ts

---
+++ Day_51/Part_2/Project/App/index.ts
@@ -0,0 +1,8 @@
+/// <reference path="../Team-A/TeamAComponent.ts"/>
+/// <reference path="../Team-B/TeamBComponent.ts"/>
+
+let obj1 = new Project.TeamA.Demo();
+let obj2 = new Project.TeamA.Demo();
+
+obj1.Print();
+obj2.Print();
