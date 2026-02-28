fd458b8bf54a60ce4606bfc4e2eb18381aa6b311
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-02-28 08:42:33 +0530

    Delete Day_51 directory

--- Day_51/library/contracts/ProductContract.ts
+++
@@ -1,7 +0,0 @@
-export interface ProductContract {
-  Name: string;
-  Price: number;
-  Qty: number;
-  Total(): number;
-  Print(): void;
-}
--- Day_51/library/templates/ProductTemplate.ts
+++
@@ -1,8 +0,0 @@
-import { ProductContract } from "../contracts/ProductContract";
-export abstract class ProductTemplate implements ProductContract {
-  Name: string = "";
-  Price: number = 0;
-  Qty: number = 0;
-  abstract Total(): number;
-  abstract Print(): void;
-}
