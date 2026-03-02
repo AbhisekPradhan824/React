b60623d73f7f4986d6afdb6522897c4efebd2530
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-03-03 01:13:50 +0530

    Update user-login.jsx

--- Day_54/Part-1/src/components/login/user-login.jsx
+++ Day_54/Part-1/src/components/login/user-login.jsx
@@ -1,9 +1,16 @@
 import { useCaptcha } from "../../hooks/captcha";
+import { useApi } from "../../hooks/getapi";
 
 export function UserLogin() {
   const captcha = useCaptcha();
+  const categories = useApi("https://fakestoreapi.com/products/categories");
   return (
     <div className="container-fluid">
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
       <h3>User Login</h3>
       <dl>
         <dt>UserId</dt>
