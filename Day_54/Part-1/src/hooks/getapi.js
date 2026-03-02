5c7704cc914814531d9f499eb9e7095ff2f655b7
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-03-03 01:13:25 +0530

    Create getapi.js

---
+++ Day_54/Part-1/src/hooks/getapi.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+export function useApi(url) {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((responseData) => {
+        setData(responseData);
+      });
+  }, [url]);
+  return data;
+}
